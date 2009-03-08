23b7c3b6499cb9b10b5ad046a52f5f723dbf3039
Sven Lange <sv3n.lange@gmail.com>
2009-03-08 15:59:12 +0000

    GRAILSPLUGINS-971 - New code attribute for tip tag for i18n

--- grails-app/taglib/TooltipTagLib.groovy
+++ grails-app/taglib/TooltipTagLib.groovy
@@ -1,18 +1,22 @@
 class TooltipTagLib {
 
-  static namespace = "tooltip"
+    static namespace = "tooltip"
 
-  def resources = {attrs ->
-    out << """
+    def resources = {attrs ->
+        out << """
         <link rel="stylesheet" href="${createLinkTo(dir: pluginContextPath, file: 'css/tooltip/default/tooltip.css')}"/>
         <script type="text/javascript" src="${createLinkTo(dir: pluginContextPath, file: "js/tooltip/tooltip-min.js")}"></script>
            """
-  }
+    }
 
-  def tip = {attrs, body ->
-    assert attrs.value
-    attrs.value = attrs.value.trim().encodeAsHTML()
-    out << """<span onmouseover="tooltip.show('${attrs.value}');" onmouseout="tooltip.hide();">${body()}</span>"""
-  }
+    def tip = {attrs, body ->
+        if (!attrs.value && !attrs.code)
+            throwTagError("Tag [tooltip:tip] is missing required attribute [code] or [value]")
+        
+        def tooltip = attrs.code ? g.message(code: "${attrs.code}") : attrs.value
+        tooltip = tooltip.encodeAsHTML()
+
+        out << """<span onmouseover="tooltip.show('${tooltip}');" onmouseout="tooltip.hide();">${body()}</span>"""
+    }
 
 }
--- test/unit/TooltipTagLibTests.groovy
+++ test/unit/TooltipTagLibTests.groovy
@@ -1,15 +1,53 @@
-import grails.test.*
+import org.codehaus.groovy.grails.plugins.codecs.HTMLCodec
+import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException
 
-class TooltipTagLibTests extends TagLibUnitTestCase {
-    protected void setUp() {
-        super.setUp()
+class TooltipTagLibTests extends grails.test.TagLibUnitTestCase {
+
+    void testTipValue() {
+        def mockString = mockFor(String)
+        mockString.demand.encodeAsHTML(1..1) { HTMLCodec.encode(delegate) }
+
+        tagLib.tip([value: '<someTag>tzu</someTag>']) {"////&\\\\"}
+        def expected = """<span onmouseover="tooltip.show('&lt;someTag&gt;tzu&lt;/someTag&gt;');" onmouseout="tooltip.hide();">////&\\\\</span>"""
+        assertEquals expected, tagLib.out.toString()
     }
 
-    protected void tearDown() {
-        super.tearDown()
+    void testTipValueWithUmlauts() {
+        def mockString = mockFor(String)
+        mockString.demand.encodeAsHTML(1..1) { HTMLCodec.encode(delegate) }
+
+        tagLib.tip([value: 'φόδ']) {"someTextThatWillShowTheTooltip"}
+        def expected = """<span onmouseover="tooltip.show('&ouml;&uuml;&auml;');" onmouseout="tooltip.hide();">someTextThatWillShowTheTooltip</span>"""
+        assertEquals expected, tagLib.out.toString()
     }
 
-    void testSomething() {
+    void testTipCode() {
+        def myI18nMessage = "myI18nMessage"
+        def mockString = mockFor(String)
+        mockString.demand.encodeAsHTML(1..1) { HTMLCodec.encode(delegate) }
+        tagLib.metaClass.message = {myI18nMessage}
 
+        tagLib.tip([code: 'someCode']) {"b"}
+        def expected = """<span onmouseover="tooltip.show('$myI18nMessage');" onmouseout="tooltip.hide();">b</span>"""
+        assertEquals expected, tagLib.out.toString()
     }
+
+    void testTipCodeAndValue() {
+        def myI18nMessage = "myI18nMessage"
+        def mockString = mockFor(String)
+        mockString.demand.encodeAsHTML(1..1) { HTMLCodec.encode(delegate) }
+        tagLib.metaClass.message = {myI18nMessage}
+
+        tagLib.tip([code: 'someCodeReturningMyI18nMessage', value: 'someValue']) {"b"}
+        def expected = """<span onmouseover="tooltip.show('$myI18nMessage');" onmouseout="tooltip.hide();">b</span>"""
+        assertEquals expected, tagLib.out.toString()
+    }
+
+    void testTipWithoutCodeOrValueAttribute() {
+        def msg = shouldFail(GrailsTagException) {
+            tagLib.tip([:]) {"b"}
+        }
+        assertEquals "Tag [tooltip:tip] is missing required attribute [code] or [value]", msg
+    }
+
 }
