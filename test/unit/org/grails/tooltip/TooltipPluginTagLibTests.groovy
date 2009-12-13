aca50cb6ca130857e5742127b9d2626fe6c37542
Sven Lange <sv3n.lange@gmail.com>
2009-12-13 17:56:06 +0000

    GRAILSPLUGINS-1617 Placed taglib into package

--- test/unit/org/grails/tooltip/TooltipPluginTagLibTests.groovy
+++ test/unit/org/grails/tooltip/TooltipPluginTagLibTests.groovy
@@ -1,6 +1,28 @@
+/* Copyright 2009 Sven Lange
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.grails.tooltip
+
 import org.codehaus.groovy.grails.plugins.codecs.HTMLCodec
 import org.codehaus.groovy.grails.web.taglib.exceptions.GrailsTagException
 
+/**
+ * Unit test for tooltip tag lib. 
+ *
+ * @author Sven Lange
+ */
 class TooltipPluginTagLibTests extends grails.test.TagLibUnitTestCase {
 
     void setUp() {
